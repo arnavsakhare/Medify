07d9fb236b3f0c33026059b5600bc5c6b5c81a37
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-06-23 17:53:13 +0530

    HomePage

---
+++ src/pages/Homepage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box, Container, Stack } from '@mui/material'
+import Offers from '../../components/Offers/Offers'
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Offers />
+    </Box>
+  )
+}
+
+export default HomePage
