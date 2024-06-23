e97f49b6d7fe606150795364d91343b7c5f244bf
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-06-23 19:12:05 +0530

    Added the Find By Specialisation Section to HomePage

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Box, Container, Stack } from '@mui/material'
 import Offers from '../../components/Offers/Offers'
+import FindBySpecialisation from '../../components/FindBySpecialisation/FindBySpecialisation'
 
 const HomePage = () => {
   return (
     <Box>
       <Offers />
+      <FindBySpecialisation />
     </Box>
   )
 }
