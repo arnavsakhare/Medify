95da982e5c60677c8ba3ec166d1debb97c8b5e67
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-06-24 01:56:36 +0530

    Added the non interactive parts

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -4,14 +4,21 @@ import Offers from '../../components/Offers/Offers'
 import FindBySpecialisation from '../../components/FindBySpecialisation/FindBySpecialisation'
 import OurSpecialists from '../../components/OurSpecialists/OurSpecialists'
 import PatientCaring from '../../components/PatientCaring/PatientCaring'
+import BlogPost from '../../components/BlogPost/BlogPost'
+import OurFamilies from '../../components/OurFamilies/OurFamilies'
+import FAQSection from '../../components/FAQSection/FAQSection'
 
 const HomePage = () => {
   return (
     <Box>
+
       <Offers />
       <FindBySpecialisation />
       <OurSpecialists />
       <PatientCaring />
+      <BlogPost />
+      <OurFamilies />
+      <FAQSection />
     </Box>
   )
 }
