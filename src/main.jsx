ac25367ccdc6bc9c4a930c25b8d7655a196c1685
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-06-23 17:54:43 +0530

    Added routes plus theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,135 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import HomePage from './pages/Homepage/HomePage.jsx'
+import SearchPage from './pages/SearchPage/SearchPage.jsx'
+import BookingsPage from './pages/BookingsPage/BookingsPage.jsx'
+import { createTheme, ThemeProvider } from '@mui/material'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/search' element={<SearchPage />} />
+      <Route path='/bookings' element={<BookingsPage />} />
+    </Route>
+  )
+)
+
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: "Poppins , sans-serif",
+  },
+  palette: {
+    primary: {
+      main: "#2AA7FF",
+      green: "#00A500",
+      secondary: "#1B3C74",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "8px",
+          textTransform: "none",
+        },
+        contained: {
+          color: "#fff",
+        },
+      },
+    },
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          width: "90%",
+        },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        h1: {
+          color: "#000000",
+          fontSize: "56px",
+          fontWeight: "700",
+        },
+        h2: {
+          color: "#1B3C74",
+          fontWeight: "600",
+          fontSize: "48px",
+          lineHeight: 1.2,
+        },
+        h3: {
+          color: "#102851",
+          fontSize: "30px",
+          fontWeight: "500",
+        },
+      },
+    },
+    MuiSelect: {
+      styleOverrides: {
+        root: {
+          background: "#FAFBFE",
+          borderRadius: "8px",
+          color: "#ABB6C7",
+          "& .MuiOutlinedInput-notchedOutline, &:hover .MuiOutlinedInput-notchedOutline, & .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+            {
+              borderColor: "#F0F0F0",
+            },
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          background: "#FAFBFE",
+          borderRadius: "8px",
+          color: "#ABB6C7",
+          "& .MuiOutlinedInput-notchedOutline, &:hover .MuiOutlinedInput-notchedOutline, & .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+            {
+              borderColor: "#F0F0F0",
+            },
+        },
+      },
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+theme.typography.h2 = {
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "32px",
+  },
+};
+
+theme.typography.h1 = {
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "36px",
+  },
+};
+
+theme.typography.h3 = {
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "22px",
+  },
+};
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router}/>
+    </ThemeProvider>
   </React.StrictMode>,
 )
