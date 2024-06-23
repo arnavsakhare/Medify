56466344e5f3af08288181cbc249961a58b7c8f0
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-06-23 20:21:48 +0530

    Added the Patients Caring Section

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Box, Container, Stack } from '@mui/material'
 import Offers from '../../components/Offers/Offers'
 import FindBySpecialisation from '../../components/FindBySpecialisation/FindBySpecialisation'
+import OurSpecialists from '../../components/OurSpecialists/OurSpecialists'
+import PatientCaring from '../../components/PatientCaring/PatientCaring'
 
 const HomePage = () => {
   return (
     <Box>
       <Offers />
       <FindBySpecialisation />
+      <OurSpecialists />
+      <PatientCaring />
     </Box>
   )
 }
