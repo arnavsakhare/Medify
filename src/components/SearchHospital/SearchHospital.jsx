700a30b3a1fc37593647d00c8f688ace4f50730e
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-06-25 01:16:57 +0530

    Search implementation

---
+++ src/components/SearchHospital/SearchHospital.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchHospital = () => {
+  return (
+    <div>SearchHospital</div>
+  )
+}
+
+export default SearchHospital
--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -8,12 +8,28 @@ import BlogPost from '../../components/BlogPost/BlogPost'
 import OurFamilies from '../../components/OurFamilies/OurFamilies'
 import FAQSection from '../../components/FAQSection/FAQSection'
 import NavBar from '../../components/NavBar/NavBar'
+import HeroSection from '../../components/HeroSection/HeroSection'
+import SearchHospital from '../../components/SearchHospital/SearchHospital'
+import Services from '../../components/Services/Services'
 
 const HomePage = () => {
   return (
     <Box>
       <Box mb={4}>
         <NavBar />
+        <Container maxWidth='xl'>
+          <HeroSection />
+
+          <Stack
+            p={{xs: 2, md: 6}}
+            bgcolor='#fff'
+            borderRadius='15px'
+            boxShadow='0 0 12px rgba(0,0,0,0.1)'
+          >
+            <SearchHospital/>
+            <Services />
+          </Stack>
+        </Container>
       </Box>
 
       <Offers />
