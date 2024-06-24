76e025616385e46deb4691e0b2d480a011a66985
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-06-25 03:31:44 +0530

    added search bar

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -9,24 +9,25 @@ import OurFamilies from '../../components/OurFamilies/OurFamilies'
 import FAQSection from '../../components/FAQSection/FAQSection'
 import NavBar from '../../components/NavBar/NavBar'
 import HeroSection from '../../components/HeroSection/HeroSection'
-import SearchHospital from '../../components/SearchHospital/SearchHospital'
+import SearchBar from '../../components/SearchBar/SearchBar'
 import Services from '../../components/Services/Services'
 
 const HomePage = () => {
   return (
     <Box>
-      <Box mb={4}>
+      <Box mb={4} sx={{background: "linear-gradient(#E7F0FF, rgba(232, 241, 255, 0.47) 85%, #fff 15%)"}}>
         <NavBar />
         <Container maxWidth='xl'>
           <HeroSection />
 
           <Stack
             p={{xs: 2, md: 6}}
+            gap={7}
             bgcolor='#fff'
             borderRadius='15px'
             boxShadow='0 0 12px rgba(0,0,0,0.1)'
           >
-            <SearchHospital/>
+            <SearchBar/>
             <Services />
           </Stack>
         </Container>
@@ -44,3 +45,5 @@ const HomePage = () => {
 }
 
 export default HomePage
+
+
