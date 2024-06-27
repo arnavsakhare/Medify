e106f5a99903ab361267bc49eb1b264373001fb8
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-06-27 22:38:39 +0530

    Implemented Bookings Page

--- src/pages/BookingsPage/BookingsPage.jsx
+++ src/pages/BookingsPage/BookingsPage.jsx
@@ -1,8 +1,91 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import cta from '../../assets/cta.png'
+import NavBar from '../../components/NavBar/NavBar'
+import { Box, Container, Stack, Typography } from '@mui/material'
+import BookingsSearchBar from '../../components/BookingsSearchBar/BookingsSearchBar'
+import HospitalCard from '../../components/HospitalCard/HospitalCard'
+
 
 const BookingsPage = () => {
+
+  const [bookings, setBookings] = useState([])
+  const [filteredBookings, setFilteredBookings] = useState([])
+
+  useEffect(() => {
+    const localBookings = localStorage.getItem("bookings") || "[]"
+    setBookings(JSON.parse(localBookings))
+  }, [])
+
+  useEffect(() => {
+    setFilteredBookings(bookings)
+  }, [bookings])
+
+
   return (
-    <div>BookingsPage</div>
+    <>
+      <NavBar />
+
+      <Box sx={{background: "linear-gradient(#EFF5FE, rgba(241, 247, 255, 0.5))"}}>
+        <Box
+          mb={4}
+          pt={{xs: 3, md: 1}}
+          sx={{
+            position: 'relative',
+            background: "linear-gradient(90deg, #2AA7FF, #0C8CE5)",
+            borderRadius: "0 0 10px 10px"
+          }}
+        >
+          <Container maxWidth='xl' sx={{px: {xs: 0, md: 5}}}>
+            <Stack direction={{xs: 'column', md: 'row'}} spacing={{xs: 0, md: 12}} alignItems={{xs: 'center', md: 'flex-end'}}>
+              <Typography component='h1' pb={1} fontSize={{xs: 32, md: 40}} fontWeight={600} color='#fff'>
+                My Bookings
+              </Typography>
+
+              <Box
+                bgcolor='#fff'
+                p={3}
+                flexGrow={1}
+                borderRadius={2}
+                boxShadow='0 0 10px rgba(0,0,0,0.1)'
+                sx={{translate: "0 50px"}}
+              >
+                <BookingsSearchBar bookings={bookings} setFilteredBookings={setFilteredBookings}/>
+              </Box>
+            </Stack>
+          </Container>
+        </Box>
+
+        <Container maxWidth='xl' sx={{pt: 10, pb: 10, px: {xs: 0, md: 4}}}>
+          <Stack alignItems='flex-start' direction={{md: 'row'}}>
+            <Stack
+              mb={{xs: 4, md: 0}}
+              spacing={3}
+              width={{xs: 1, md: "calc(100% - 384px)"}}
+              mr='24px'
+            >
+              {filteredBookings.length > 0 && (
+                filteredBookings.map((hospital) => (
+                  <HospitalCard
+                    key={hospital["Hospital Name"]}
+                    details={hospital}
+                    booking={true}
+                  />
+                ))
+              )}
+
+              {filteredBookings.length == 0 && (
+                <Typography variant='h3' bgcolor='#fff' p={3} borderRadius={2}>
+                  No Bookings Found
+                </Typography>
+              )}
+
+            </Stack>
+
+            <img src={cta} alt="ad" width={360} height={250} />
+          </Stack>
+        </Container>
+      </Box>
+    </>
   )
 }
 
